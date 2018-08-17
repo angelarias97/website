4f9984d9cfe51fa9a1d61c09ecf5e7b9d31b0730
angel arias <angelarias97@gmail.com>
2018-08-17 18:58:06 -0300

    add logout

--- src/App.js
+++ src/App.js
@@ -4,11 +4,52 @@ import './App.css';
 
 class App extends Component {
 
+  constructor(){
+    super();
+    this.state={
+      user: null
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+    this.renderLoginButton = this.renderLoginButton.bind(this);
+
+  }
+
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState ({user });
+    })
+  }
+
   handleAuth () {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
       .then(result => console.log(`${result.user.email} ha iniciado sesión`))
       .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout (){
+    firebase.auth().signOut()
+    .then(result => console.log(`${result.user.email} ha salido`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  renderLoginButton(){
+    if(this.state.user){
+      return (
+        <div>
+          <img src = {this.state.user.photoURL} alt = {this.state.user.displayName}/>
+          <p>hola {this.state.user.displayName}!</p>
+          <button onClick = {this.handleLogout}>salir</button>
+        </div>
+      );
+    }else{
+      return(
+        <button onClick={this.handleAuth}>login con Google</button>
+      );
+      
+    }
   }
 
   render() {
@@ -18,7 +59,7 @@ class App extends Component {
           <h2>WebSite</h2>
         </div>
         <p className="App-intro">
-          <button onClick={this.handleAuth}>login con Google</button>
+          {this.renderLoginButton()}
         </p>
       </div>
     );
